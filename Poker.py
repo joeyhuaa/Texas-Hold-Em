6c5ae4bf96a1ba433470ea30062d8cec3438f35c
Joey Hua <46336318+joeyhuaa@users.noreply.github.com>
2019-04-21 00:35:04 -0700

    Update Poker.py

--- Poker.py
+++ Poker.py
@@ -3,41 +3,98 @@
 # Purely mathematical outcomes
 # Can run as many simulations as user inputs
 
-from Cards import Deck
-
 # deal random hands to all players (2)
 # simulate run out till showdown
 # keep win counter for all players
 # display win % at the end
 # sort hands in ascending order from left to right
+import sys
+from PokerHand import PokerHand
+from Cards import Card
+from Cards import Deck
+deck = Deck()
+deck.shuffle()
 
-def outcome(hands=[]):
+# takes user input and returns cards as a PokerHand object
+def get_starting_hand(input):
+    if input == 'r':
+        return PokerHand([deck.deal(), deck.deal()])
+    else:
+        card1 = Card(input.split()[0][0], input.split()[0][1])
+        card2 = Card(input.split()[1][0], input.split()[1][1])
+        return PokerHand([card1, card2])
 
+# combines hole cards and community cards to find the best 5-card hand
+def get_best_hand(holecards, commcards):
+    from itertools import combinations
+
+    # repeatedly take 5-card combinations from the 7-card hand
+    seven_cards = holecards + commcards
+    seven_cards.sort()
+    five_card_combos = list(combinations(seven_cards, 5))
+
+    # since five_card_combos is a list of tuples,
+    # must turn into a list of PokerHand objects
+    hands = [PokerHand(list(hand_tuple)) for hand_tuple in five_card_combos]
+
+    # insertion sort hands by rank
+    for i in range(1, len(hands)):
+        j = i - 1
+
+        while j >= 0 and hands[i].rcmp(hands[j]) < 0:
+            hands[i], hands[j] = hands[j], hands[i]
+            j -= 1
+            i -= 1
+
+    # return the last hand because it's the best hand??
+    return hands[len(hands)-1]
+
+def display_outcome(hero, vill):
+    print('Hero hole cards: '+str(hero_hand))
+    print('Vill hole cards: '+str(vill_hand))
+    print('Community cards: '+str(community))
+
+    result = hero.rcmp(vill)
+
+    if result > 0:
+        print('Hero wins with ' + hero.get_rank())
+    elif result < 0:
+        print('Villain wins with ' + vill.get_rank())
+    else:
+        print('Tie')
 
 def simulate():
-
-    # input
     try:
-        num_games = input('Number of games: ')
+        # input
+        hero_holecards = input('Enter hole cards for hero, or "r" for a random hand: ')
+        vill_holecards = input('Enter hole cards for villain, or "r" for a random hand: ')
+        # num_games = input('Number of games: ')
 
-        # deal hands
-        for i in range(1,num_games+1):
+        # retrieve starting hands
+        global hero_hand
+        hero_hand = get_starting_hand(hero_holecards)
+        global vill_hand
+        vill_hand = get_starting_hand(vill_holecards)
 
-            # shuffle deck
-            deck = Deck()
-            deck.shuffle()
+        # deal community cards
+        global community
+        community = PokerHand([deck.deal() for i in range(5)])
 
-            # deal hands and community cards
-            hero_hand = [deck.deal(), deck.deal()]
-            vill_hand = [deck.deal(), deck.deal()]
-            community = [deck.deal(), deck.deal(), deck.deal(), deck.deal(), deck.deal()]
+        # find the best hand
+        hero_besthand = get_best_hand(hero_hand, community)
+        vill_besthand = get_best_hand(vill_hand, community)
 
-            # form the best possible 5-card hands for each player
-            # linear search for repetitions
-            for
-
+        # display the winner
+        display_outcome(hero_besthand, vill_besthand)
 
     except TypeError:
-        print('Just enter a goddamn number dude.')
+        print('Just enter a goddamn number.')
+    except EOFError:
+        sys.exit(0)
+
+def main():
+    simulate()
+
+main()
 
 
